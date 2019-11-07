ab777dbb13b7d18fbdb8ed4244cd07f611045caf
Anton Romankov <a.romankov@fil-it.ru>
2019-11-07 15:16:03 +0300

    Add ajax send form

---
+++ dist/mail.php
@@ -0,0 +1,49 @@
+<?php
+sleep(3);
+$to = "ar@askque.ru";
+$subject = "Заполнена форма обратной связи на сайте Автодоп";
+$from = 'noreply@autodop-spb.ru';
+
+$json = file_get_contents('php://input');
+$decoded = json_decode($json, true);
+
+$dictionary = [
+    "name" => "Имя",
+    "phone" => "Телефон",
+    "car-brand" => "Марка автомобиля",
+    "car-model" => "Модель автомобиля",
+    "salon" => "Салон",
+    "time" => "Удобное время",
+];
+
+$content = '';
+
+foreach ($decoded as $key => $value) {
+    if ($value !== NULL){
+        $content .= '<p><strong>' . $dictionary[$key] . ':</strong> ' . $value .'</p>';
+    }
+}
+
+echo $content;
+
+$message = '
+<html>
+<head>
+    <title>
+    ' .$subject. '
+    </title>
+</head>
+<body>
+' .$content. '
+</body>
+</html>';
+
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "Content-type: text/html; charset=utf-8 \r\n";
+$verify = mail($to, $subject, $message, "from: " . $from . "\r\n" . $headers);
+
+if ($verify) {
+    echo $message;
+} else {
+    echo 'Something got wrong...';
+}
